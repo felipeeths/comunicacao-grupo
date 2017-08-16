e60ef6143af1cef14e99e49a6196e2a4f7a0e25b
alanllino <alaan_lino@hotmail.com>
2017-08-16 16:20:16 -0300

    projeto java criado

---
+++ comunicacao/src/comunicacao/Main.java
@@ -0,0 +1,10 @@
+package comunicacao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("oi felipe\nhello world");
+	}
+
+}
